cc342580eaaa1fa14ca908b9abd765393f55544d
samraj <30625392+samrajkumar@users.noreply.github.com>
2017-08-16 11:17:52 +0530

    Add files via upload

---
+++ client/sw.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const applicationServerPublicKey = 'BAdjJip1yHHLkSoqUtjZWzd7ET8v65nP7MJ7FL9JUpOJ0bZEWbLK9U8MjkeCH58H0TSLWKQPg6ari4SAKW6k4jU';
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Nibana Solutions';
+  const options = {
+    body: 'Message Received \n ' + event.data.text(),
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('http://www.nibanasolutions.com/')
+  );
+});
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  event.waitUntil(
+    self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function(newSubscription) {
+      console.log('[Service Worker] New subscription: ', newSubscription);
+    })
+  );
+});
